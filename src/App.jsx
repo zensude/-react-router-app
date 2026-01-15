00f101c19bec4b037dbbfac064f8cb31d1b27a89
Zeynep <zsude8408@gmail.com>
2026-01-15 17:53:46 +0300

    router pages + navbar

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </div>
+    );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <nav style={{ display: "flex", gap: "12px", padding: "10px" }}>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
+    );
+}
