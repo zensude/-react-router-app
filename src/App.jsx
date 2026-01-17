59539ff600a73a1a3a775d617f0db3da2c98523a
Zeynep <zsude8408@gmail.com>
2026-01-18 00:45:48 +0300

    Add conditional rendering and state logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
+import Numbers from "./pages/Numbers";
 
 export default function App() {
     return (
@@ -14,6 +15,7 @@ export default function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/numbers" element={<Numbers />} />
             </Routes>
         </div>
     );
---
+++ src/pages/Numbers.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function Numbers() {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const evenNumbers = numbers.filter(num => num % 2 === 0);
+    const multiplied = evenNumbers.map(num => num * 10);
+    const total = multiplied.reduce((acc, num) => acc + num, 0)
+    const [showEvens, setShowEvens] = useState(false);
+    return (
+        <div style={{ padding: 20 }} >
+            <h2>Numbers</h2>
+            <button onClick={() => setShowEvens(!showEvens)}>{showEvens ? "Hide evens" : "Show evens"} </button>
+            {showEvens && (
+                <>
+                    {multiplied.map((num) => (
+                        <p key={num}>{num}</p>
+                    ))}
+                    <strong>Total: {total}</strong>
+                </>
+            )}
+        </div>
+    );
+}
