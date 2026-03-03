f7df7e04202557c82ca0ff03958806a68a9869ad
Zeynep <zsena857@gmail.com>
2026-03-04 00:00:52 +0300

    feat: highlight active navbar link using NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,16 +13,24 @@
 //     );
 // }
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+    const linkStyle = ({ isActive }) => ({
+        textDecoration: "none",
+        color: isActive ? "white" : "black",
+        backgroundColor: isActive ? "black" : "transparent",
+        padding: "6px 10px",
+        borderRadius: "6px",
+    });
+
     return (
         <nav style={{ display: "flex", gap: "12px", padding: "10px" }}>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/numbers">Numbers</Link>
-            <Link to="/users">Users</Link>
+            <NavLink to="/" style={linkStyle}>Home</NavLink>
+            <NavLink to="/about" style={linkStyle}>About</NavLink>
+            <NavLink to="/contact" style={linkStyle}>Contact</NavLink>
+            <NavLink to="/numbers" style={linkStyle}>Numbers</NavLink>
+            <NavLink to="/users" style={linkStyle}>Users</NavLink>
         </nav>
     );
 }
